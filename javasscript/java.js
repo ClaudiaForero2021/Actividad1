3a956e7aeff13a7ae0f7e4e76be3a4f6f3908b12
CLAUDIA PATRICIA FORERO BRAVO <CG24822@sg.co.igrupobbva>
2021-11-20 18:45:01 -0500

    Actividad4

---
+++ javasscript/java.js
@@ -0,0 +1,180 @@
+$(".Empresa1").click(
+    function () {
+        $(".Empresa1").css("font-size", "35px")    
+    }
+)
+
+$(".Empresa1").dblclick(
+    function () {
+        $(".Empresa1").css("background", "blue")
+        
+    }
+)
+
+$(".Empresa2").click(
+    function () {
+        $(".Empresa2").css("font-size", "28px")    
+    }
+)
+
+$(".Empresa2").dblclick(
+    function () {
+        $(".Empresa2").css("background", "yellow")
+        
+    }
+)
+
+$(".Empresa3").click(
+    function () {
+        $(".Empresa3").css("font-size", "35px")    
+    }
+)
+
+$(".Empresa3").dblclick(
+    function () {
+        $(".Empresa3").css("background", "red")
+        
+    }
+)
+
+$(".Empresa4").click(
+    function () {
+        $(".Empresa4").css("font-size", "20px")    
+    }
+)
+
+$(".Empresa4").dblclick(
+    function () {
+        $(".Empresa4").css("background", "green")
+        
+    }
+)
+
+$(".Empresa5").click(
+    function () {
+        $(".Empresa5").css("font-size", "10px")    
+    }
+)
+
+$(".Empresa5").dblclick(
+    function () {
+        $(".Empresa5").css("background", "pink")
+        
+    }
+)
+
+$(".Empresa6").click(
+    function () {
+        $(".Empresa6").css("font-size", "38px")    
+    }
+)
+
+$(".Empresa6").dblclick(
+    function () {
+        $(".Empresa6").css("background", "purple")
+        
+    }
+)
+
+$(".Empresa7").click(
+    function () {
+        $(".Empresa7").css("font-size", "25px")    
+    }
+)
+
+$(".Empresa7").dblclick(
+    function () {
+        $(".Empresa7").css("background", "yellow")
+        
+    }
+)
+
+$(".Empresa8").click(
+    function () {
+        $(".Empresa8").css("font-size", "20px")    
+    }
+)
+
+$(".Empresa8").dblclick(
+    function () {
+        $(".Empresa8").css("background", "orange")
+        
+    }
+)
+
+$(".Empresa9").click(
+    function () {
+        $(".Empresa9").css("font-size", "65px")    
+    }
+)
+
+$(".Empresa9").dblclick(
+    function () {
+        $(".Empresa9").css("background", "blue")
+        
+    }
+)
+
+$(".Empresa10").click(
+    function () {
+        $(".Empresa10").css("font-size", "30px")    
+    }
+)
+
+$(".Empresa10").dblclick(
+    function () {
+        $(".Empresa10").css("background", "indigo")
+        
+    }
+)
+
+
+
+
+
+
+
+$(".parrafo1").mouseover(
+    function() {
+        $(".parrafo1").css("background", "pink")
+        
+    }
+)    
+
+$(".parrafo1").mouseout(
+    function() {
+        $(".parrafo1").css("background", "white")
+        
+    }
+)   
+
+
+$(".parrafo2").mouseover(
+    function() {
+        $(".parrafo2").css("background", "blue")
+        
+    }
+)    
+
+$(".parrafo2").mouseout(
+    function() {
+        $(".parrafo2").css("background", "green")
+        
+    }
+)   
+
+$(".parrafo3").mouseover(
+    function() {
+        $(".parrafo3").css("background", "red")
+        
+    }
+)    
+
+$(".parrafo3").mouseout(
+    function() {
+        $(".parrafo3").css("background", "white")
+        
+    }
+)  
+
+
